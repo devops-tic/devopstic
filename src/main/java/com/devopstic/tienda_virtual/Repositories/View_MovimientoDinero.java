022fa5060f02ebba6f9cef262b6a9cdc408a2b29
JhonMeza96 <estebanmezaher96@gmail.com>
2022-09-22 17:51:37 -0500

    Se realiza merge sobre la rama Development para actualizar la rama con el trabajo realizado en Spring3 por el compañero @EduardHumanez

--- src/main/java/com/devopstic/tienda_virtual/Repositories/View_MovimientoDinero.java
+++ src/main/java/com/devopstic/tienda_virtual/Repositories/View_MovimientoDinero.java
@@ -1,11 +1,9 @@
 package com.devopstic.tienda_virtual.Repositories;
 
-<<<<<<< HEAD
-public interface View_MovimientoDinero {
-=======
+
 import com.devopstic.tienda_virtual.Model.MovimientoDinero;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface View_MovimientoDinero extends JpaRepository<MovimientoDinero,Integer> {
->>>>>>> spring3
+
 }
